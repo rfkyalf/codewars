71e635d8103028682a33f4f07764cffa8fdca25f
Rifky Alfarez <207006052@student.unsil.ac.id>
2025-01-09 08:07:11 +0700

    String repeat

---
+++ 8kyu/string-repeat.js
@@ -0,0 +1,10 @@
+// title String repeat
+// url https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+
+// function repeatStr(n, s) {
+//   return s.repeat(n);
+// }
+
+const repeatStr = (n, s) => s.repeat(n);
+
+console.log(repeatStr(3, 'tes'));
