c01ea346011c2c82baa471f2d01dc319b19fff9e
Rifky Alfarez <207006052@student.unsil.ac.id>
2025-01-09 10:25:18 +0700

    Keep Hydrated!

---
+++ 8kyu/stay-hydrate.js
@@ -0,0 +1,10 @@
+// title Keep Hydrated!
+// url https://www.codewars.com/kata/582cb0224e56e068d800003c
+
+// function litres(time) {
+//   return Math.floor(time * 0.5);
+// }
+
+const litres = (time) => Math.floor(time * 0.5);
+
+console.log(litres(11.8));
